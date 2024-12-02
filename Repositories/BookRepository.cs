d84b6225eb37a984720b66e55e8d4426b3d33e27
Tiziano Bruno <brntiziano96@gmail.com>
2024-12-02 13:29:00 +0100

    code reformat and add book table

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace exercise_db_connection.Repositories
+{
+    public class BookRepository
+    {
+
+        private readonly AppDbContext appContext;
+
+        public BookRepository(AppDbContext context)
+        {
+            appContext = context;
+        }
+     
+        internal async Task<List<Book>> GetAll()
+        {
+            var books = await appContext.Books.ToListAsync();
+            return books;
+        }
+    }
+}

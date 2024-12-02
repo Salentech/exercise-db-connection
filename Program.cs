99eb054fa21e52ed4ce32049cf792236c67a151f
Ema De M <47746088+Raevaen@users.noreply.github.com>
2024-12-02 12:55:17 +0100

    Add AppDbContext service

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
 
 builder.Configuration.AddUserSecrets<Program>();
 
